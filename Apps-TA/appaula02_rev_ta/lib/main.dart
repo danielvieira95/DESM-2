526beb4281728dd0db01c9a6c9cd9bf67f55ee5f
danielvieira95 <engdanielvieira95@gmail.com>
2024-02-08 20:03:35 -0300

    Convertendo Stateless em Statefull Widget

--- Apps-TA/appaula02_rev_ta/lib/main.dart
+++ Apps-TA/appaula02_rev_ta/lib/main.dart
@@ -5,9 +5,15 @@ void main() {
 }
 
 // Classe Home do tipo Stateless
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String? texto;
   @override
   Widget build(BuildContext context) {
            // Material app necessário para construir os widgets
@@ -22,16 +28,22 @@ class Home extends StatelessWidget {
           Container(color: Colors.red,
           width: 200,
           height:250,  
-          child: Text("Senai",style: TextStyle(fontSize: 30),),      
+          child: Text("${texto}",style: TextStyle(fontSize: 30),),      
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(onPressed: (){
                 print("Botão pressionado");
+                setState(() {
+                  texto = "Senai Roberto Mange";
+                });
               }, child: Text("Mensagem")),
               ElevatedButton(onPressed: (){
             print("SENAI");
+             setState(() {
+                  texto = "Mobile 2";
+                });
           }, child: Text("SENAI")),
             ],
           ),
