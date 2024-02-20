62af1842d4155af6caf656e123c26cf8def2e63a
danielvieira95 <engdanielvieira95@gmail.com>
2024-02-20 19:51:13 -0300

    Tela3

--- Apps-TB/app_aula03_telas_multiplas/lib/tela3.dart
+++ Apps-TB/app_aula03_telas_multiplas/lib/tela3.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
-class Telasecundaria extends StatelessWidget {
-  const Telasecundaria({super.key});
+class Telaop extends StatelessWidget {
+  const Telaop({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      title: Text("Tela 2"),
+      title: Text("Tela opções"),
       ),
      body: Column(
       children: [
-        Container(color: Colors.red,width: 400,height: 180,),
+        Container(color: Colors.black,width: 400,height: 180,),
         ElevatedButton(onPressed: (){
         Navigator.pop(context);
 
-        }, child: Text("Tela 1")),
+        }, child: Text("Tela 2")),
       ],
      ),
 
