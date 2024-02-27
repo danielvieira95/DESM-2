0aff148a4f3ce203bbddd0126f54d283538fb723
danielvieira95 <engdanielvieira95@gmail.com>
2024-02-27 19:30:35 -0300

    Classe do tipo statefull navigator button

--- Apps-TB/aula04_navigatorbar/lib/main.dart
+++ Apps-TB/aula04_navigatorbar/lib/main.dart
@@ -24,9 +24,55 @@ class NavBottom extends StatelessWidget {
       ),
       body: Column(
         children: [
-          
+
         ],
       ),
     );
+  }
+}
+// Classe do tipo stateful 
+class NavigatorScreen extends StatefulWidget {
+  const NavigatorScreen({super.key});
+  
+  @override
+  // Variavel state que monitora os widgets da classe
+  State<NavigatorScreen> createState() => _NavigatorScreenState();
+}
+
+class _NavigatorScreenState extends State<NavigatorScreen> {
+  int selectIndex =0;
+  // TextStyle tipo de variavel para receber parametros de acordo com o tipo da fonte do texto
+  static const TextStyle optionStyle = TextStyle(fontSize:30,fontWeight: FontWeight.bold);
+  // Cria uma lista de widgets para utilizar no navigator bar
+  static const List<Widget> _widgetOptions= <Widget> [
+    Text('Index 0 : Home',style:optionStyle ),
+    Text('Index 1: Escola',style:optionStyle ),
+    Text('Index 2: Negócio',style: optionStyle,),
+    Text('Index 3: Configurações',style:optionStyle),
+
+  ];
+  // Funçao que muda o index conforme o icone é pressionado
+  void showItemTrap(int index){
+    setState(() {
+      selectIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
