7188df06a5d4933869bf48db924bf6acbd9e09cc
danielvieira95 <engdanielvieira95@gmail.com>
2024-03-12 19:53:42 -0300

    api consulta cep

--- Apps-TB/aula06_api_http_tb/lib/home.dart
+++ Apps-TB/aula06_api_http_tb/lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http; // permite os metodos para
+// consumo da api
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -8,6 +9,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // async é porque a comunicação é assincrona
+   _consultaCep () async {
+    String url = "https://cep.awesomeapi.com.br/13060354";
+    // api para consultar o endereço através do cep
+    http.Response response; // variavel para armazenar a resposta da api
+    response = await http.get(Uri.parse(url)); // response armazena a resposta da api
+    print("Codigo de status da API: ${response.statusCode.toString()}"); // codigo de resposta da api
+    print("Resposta da api ${response.body}"); // resposta da api
+    
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +29,7 @@ class _HomeState extends State<Home> {
       ),
       body: Column(
         children: [
+          ElevatedButton(onPressed: _consultaCep, child: Text("Verificar"))
 
         ],
       ),
