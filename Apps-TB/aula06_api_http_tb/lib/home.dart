130c3c0c6e6f1b615c73d7c3ade2127a49227d7a
danielvieira95 <engdanielvieira95@gmail.com>
2024-03-12 22:43:01 -0300

    permissao para autorizar acesso a internet

--- Apps-TB/aula06_api_http_tb/lib/home.dart
+++ Apps-TB/aula06_api_http_tb/lib/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http; // permite os metodos para
-// consumo da api
+import 'package:http/http.dart' as http; // permite os metodos para  consumo da api
+import 'dart:convert'; // pacote que permite a conversao dos dados
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -9,16 +9,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController ncep = TextEditingController();
+  String ? logradouro;
+    String? bairro;
+    String ? cidade;
+    String ? estado;
+    String? ddd;
   // async é porque a comunicação é assincrona
    _consultaCep () async {
-    String url = "https://cep.awesomeapi.com.br/13060354";
+    String url = "https://cep.awesomeapi.com.br/${ncep.text}";
     // api para consultar o endereço através do cep
     http.Response response; // variavel para armazenar a resposta da api
     response = await http.get(Uri.parse(url)); // response armazena a resposta da api
     print("Codigo de status da API: ${response.statusCode.toString()}"); // codigo de resposta da api
-    print("Resposta da api ${response.body}"); // resposta da api
+    //print("Resposta da api ${response.body}"); // resposta da api
     
 
+    Map<String,dynamic>dados = json.decode(response.body);
+    setState(() {
+    logradouro = dados["address"];
+    bairro = dados["district"];
+    cidade= dados["city"];
+    estado = dados["state"];
+    ddd = dados["ddd"];
+      
+    }); // variavel para armazenar os dados da api
+    
+    print("Logradouro: $logradouro");
+    print("Bairro: $bairro");
+    print("cidade: $cidade - $estado");
+    print("ddd: $ddd");
   }
   @override
   Widget build(BuildContext context) {
@@ -28,9 +48,23 @@ class _HomeState extends State<Home> {
         
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ElevatedButton(onPressed: _consultaCep, child: Text("Verificar"))
-
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: "Digite o cep"),
+              controller: ncep ,
+          
+               
+            ),
+          ),
+          Center(child: ElevatedButton(onPressed: _consultaCep, child: Text("Verificar"))),
+          Text("Logradouro: $logradouro ",style: TextStyle(fontSize: 18),),
+          Text("Bairro: $bairro ",style: TextStyle(fontSize: 18),),
+          Text("Cidade: $cidade - $estado ",style: TextStyle(fontSize: 18),),
+          Text("DDD: $ddd ",style: TextStyle(fontSize: 18),),
         ],
       ),
     );
