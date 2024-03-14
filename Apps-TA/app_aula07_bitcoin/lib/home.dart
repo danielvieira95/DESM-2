662a601a4a983ff0a66115271fea89dfead9f0e7
danielvieira95 <engdanielvieira95@gmail.com>
2024-03-14 19:45:43 -0300

    atualizacao campo parse api

--- Apps-TA/app_aula07_bitcoin/lib/home.dart
+++ Apps-TA/app_aula07_bitcoin/lib/home.dart
@@ -10,12 +10,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String preco = "0";
   _consultaBitCoin() async{
     String url = "https://blockchain.info/ticker"; // url com a api 
     http.Response response = await http.get(Uri.parse(url)) ; // espera o retorno da api
     Map<String, dynamic> dados = json.decode(response.body); // decodifica o dado da api
+    setState(() {
+       preco = dados["BRL"]["buy"].toString(); // faz o parse da informação
+    });   
     print("Resultado api ${response.statusCode.toString()}");
     print("Valores BitCoin ${dados}");
+    print("Valor do bitcoin em R\$ ${preco}");
      
   }
   @override
@@ -30,8 +35,8 @@ class _HomeState extends State<Home> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(width: 250,height: 150,color: Colors.amber,),
-           Text("Valor BitCoin R\$: ",style: TextStyle(fontSize: 20),),
-           ElevatedButton(onPressed: (){}, child: Text("Verificar")),
+           Text("Valor BitCoin R\$: $preco ",style: TextStyle(fontSize: 20),),
+           ElevatedButton(onPressed: _consultaBitCoin, child: Text("Verificar")),
           ],
         ),
       ),
