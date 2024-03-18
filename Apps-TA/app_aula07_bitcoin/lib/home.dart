2554bc18ad7fc8ee20f85ccfb8e5b5aa875c9f52
danielvieira95 <engdanielvieira95@gmail.com>
2024-03-18 20:38:47 -0300

    Atualização app bitcoin

--- Apps-TA/app_aula07_bitcoin/lib/home.dart
+++ Apps-TA/app_aula07_bitcoin/lib/home.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:http/http.dart' as http;
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -28,15 +29,84 @@ class _HomeState extends State<Home> {
     print("Valores BitCoin ${dados}");
     print("Valor do bitcoin em R\$ ${preco}");
   }
-    _calcularreais_todolar(){
-      setState(() {
-        vdolar = double.parse(valor.text) /4.99;     
+    _converter(){
+     if(selectop ==0 && selectop1 ==1)
+        {  setState(() {
+            vdolar = double.parse(valor.text); 
+        });
+            
+        }
+          else if(selectop ==0 && selectop1 ==3)
+        {
+           setState(() {
+             vdolar = double.parse(valor.text) /5.01; 
+           });
+            
+        }  
+        else if(selectop ==0 && selectop1 ==5)
+        {
+           setState(() {
+             vdolar = double.parse(valor.text)/5.45;
+           });
+             
+        } 
+         else if(selectop ==2 && selectop1 ==1)
+        {
+           setState(() {
+             vdolar = double.parse(valor.text)*5.01;
+           });
+             
+        } 
+         else if(selectop ==2 && selectop1 ==3)
+        {
+           setState(() {
+             vdolar = double.parse(valor.text);
+           });
+             
+        } 
+        else if(selectop ==2 && selectop1 ==5)
+        {
+           setState(() {
+             vdolar = double.parse(valor.text)*0.92;
+           });
+             
+        } 
+        else if(selectop ==3 && selectop1 ==1)
+        {
+           setState(() {
+             vdolar = double.parse(valor.text)*0.18;
+           });
+             
+        } 
+        else if(selectop ==4 && selectop1 ==3)
+        {
+           setState(() {
+             vdolar = double.parse(valor.text)*0.91;
+           });
+             
+        } 
+         else if(selectop ==4 && selectop1 ==5)
+        {
+           setState(() {
+             vdolar = double.parse(valor.text);
+           });
+             
+        } 
+        else if(selectop ==4 && selectop1 ==1)
+        {
+           setState(() {
+             vdolar = double.parse(valor.text)*5.45;
+           });
+             
+        } 
+
+
         
-      });
     }
-    _calcularreais_toeuro(){
+    _limpar(){
       setState(() {
-        veuro = double.parse(valor.text) /5.43;     
+        vdolar = 0;  
+        valor.text = "";   
         
       });
     }
@@ -47,90 +117,150 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text("App consulta preço BitCoin"),
-
+     
       ),
       body: 
-         Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+         ListView(
+          
+          scrollDirection:Axis.vertical,
           children: [
             Container(width: 250,height: 150,color: Colors.amber,child: Image.asset('imagens/bitcoin.png'),),
-           Text("Valor BitCoin R\$: $preco ",style: TextStyle(fontSize: 20),),
+           Padding(
+             padding: const EdgeInsets.all(50.0),
+             child: Text("Valor BitCoin R\$: $preco ",style: TextStyle(fontSize: 20),),
+           ),
+           
            TextField(
            keyboardType: TextInputType.number,
            decoration:  InputDecoration(labelText: "Digite o valor a ser convertido"),
            controller: valor,
            ),
-           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              
-            ElevatedButton(onPressed: _consultaBitCoin, child: Text("Verificar")),
-           ElevatedButton(onPressed: _calcularreais_todolar, child: Text("Calcular")),
-           ElevatedButton(onPressed: _calcularreais_toeuro, child: Text("Calcular")),
-            
-            ],
-           ),
+           
            
            Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text("Origem",style: TextStyle(fontSize: 15),),
               
-
-               
+              
               Text("Destino",style: TextStyle(fontSize: 15),),
             ],
            ),
+           Container(color: Colors.white,width: 450,height: 150,
+           child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Radio(                
+                          value: 0,
+                           groupValue: selectop, 
+                           onChanged: (value){
+                            setState(() {
+                              selectop = value;
+                            });
+                           }),
+                           Text("R\$"),
+                           Radio(                
+                          value: 1,
+                           groupValue: selectop1, 
+                           onChanged: (value){
+                            setState(() {
+                              selectop1 = value;
+                            });
+                           }),
+                           Text("R\$"),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Radio(                
+                          value: 2,
+                           groupValue: selectop, 
+                           onChanged: (value){
+                            setState(() {
+                              selectop = value;
+                            });
+                           }),
+                           Text("Dólar"),
+                           Radio(                
+                          value: 3,
+                           groupValue: selectop1, 
+                           onChanged: (value){
+                            setState(() {
+                              selectop1 = value;
+                            });
+                           }),
+                           Text("Dólar"),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Radio(                
+                          value: 4,
+                           groupValue: selectop, 
+                           onChanged: (value){
+                            setState(() {
+                              selectop = value;
+                            });
+                           }),
+                           Text("Euro"),
+                           Radio(                
+                          value: 5,
+                           groupValue: selectop1, 
+                           onChanged: (value){
+                            setState(() {
+                              selectop1 = value;
+                            });
+                           }),
+                           Text("Euro"),
+                ],
+              )
+            ],
+              
+           ),),
           
-
-           Text("Valor a ser convertido ${valor.text}",style: TextStyle(fontSize: 20),),
-            Text("Valor a  convertido  \$ ${vdolar.toStringAsFixed(2)}",style: TextStyle(fontSize: 20),),
-            Text("Valor a  convertido  ${veuro.toStringAsFixed(2)}€",style: TextStyle(fontSize: 20),), 
-           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-             children: [
-                RadioListTile<int>(
-              title:Text("R\$"),
-              value: 1,
-               groupValue: selectop, 
-               onChanged: (value){
-                setState(() {
-                  selectop = value;
-                });
-               }),
-               Container(width: 50,height: 50,color: Colors.blue,),
-               Container(width: 50,height: 50,color: Colors.red,),
-                Container(width: 50,height: 50,color: Colors.green,),
-             ],
+                           
+           
+           Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 50),
+             child: Text("Valor a ser convertido ${valor.text}",style: TextStyle(fontSize: 20),),
            ),
-           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-             children: [
-               Container(width: 50,height: 50,color: Colors.blue,),
-               Container(width: 50,height: 50,color: Colors.red,),
-                Container(width: 50,height: 50,color: Colors.green,),
-             ],
-           ),
-           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-             children: [
-               Container(width: 50,height: 50,color: Colors.blue,),
-               Container(width: 50,height: 50,color: Colors.red,),
-                Container(width: 50,height: 50,color: Colors.green,),
-             ],
-           ),
-                
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal:50 ),
+              child: Text("Valor convertido  \$ ${vdolar.toStringAsFixed(2)}",style: TextStyle(fontSize: 20),),
+            ),
             
-             
+            Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              
+            ElevatedButton(onPressed: _consultaBitCoin, child: Text("Verificar")),
+           ElevatedButton(onPressed: _converter, child: Text("Calcular")),
+           ElevatedButton(onPressed: _limpar, child: Text("limpar")),
+            
+            ],
+           ),
+           
+            
+            
+                     
+                    
                
-          ],
-        ),
+                 
+                   
+         
+             
+           
+          //Text("Opção: $selectop"),
+           // Text("Opção: $selectop1"),
+          ]),
+          );
         
-      
-    );
+        
+  
   }
 }
 
--- Apps-TA/app_aula07_bitcoin/lib/main.dart
+++ Apps-TA/app_aula07_bitcoin/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:app_aula07_bitcoin/escolha_moeda.dart';
 import 'package:app_aula07_bitcoin/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Home(),
   ));
 }
