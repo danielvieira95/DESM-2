c1c66bcb31a7f7c835ecc4e8126b39fd5bdbce2d
danielvieira95 <engdanielvieira95@gmail.com>
2024-03-19 19:16:08 -0300

    app bitcoin
    import dart:convert
    Função para consultar preço bitcoin
    http.response

--- Apps-TB/aula07_app_bitcoin/lib/home.dart
+++ Apps-TB/aula07_app_bitcoin/lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http; // importa o pacote http para consumo da api
+import 'dart:convert'; // metodo para converter dado json
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -8,6 +9,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // async é porque a função vai esperar uma resposta da api pelo metodo http.get
+  _consultaprecoBitCoin() async{
+
+    String url =  "https://blockchain.info/ticker"; // url com a api 
+    http.Response response = await http.get(Uri.parse(url));
+    Map<String,dynamic> dados = json.decode(response.body); // decodifica a informação
+    //vinda da api
+
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
