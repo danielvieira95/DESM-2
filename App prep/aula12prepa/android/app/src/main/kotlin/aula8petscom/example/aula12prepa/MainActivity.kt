a97da15f494ca78387aa4df79792c36989accb91
danielvieira95 <engdanielvieira95@gmail.com>
2024-04-22 18:02:02 -0300

    app aula12a

---
+++ App prep/aula12prepa/android/app/src/main/kotlin/aula8petscom/example/aula12prepa/MainActivity.kt
@@ -0,0 +1,6 @@
+package aula8petscom.example.aula12prepa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ App prep/aula12prepa/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp();
+  
+}
+
