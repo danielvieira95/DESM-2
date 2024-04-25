d01ff96b633b9561d466bd058268a9249361c6b7
danielvieira95 <engdanielvieira95@gmail.com>
2024-04-25 19:14:38 -0300

    update tela login

--- Apps-TA/aula13_correcaoformativa/lib/login.dart
+++ Apps-TA/aula13_correcaoformativa/lib/login.dart
@@ -8,8 +8,58 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController user = TextEditingController();
+  TextEditingController senha = TextEditingController();
+  bool exibir = false;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text("App Mercado"),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            width: 300,
+            height: 300,
+            child: Column(
+              children: [
+                TextFormField(
+                  keyboardType: TextInputType.name, // define o tipo de entrada do teclado
+                  decoration: InputDecoration(border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  icon: Icon(Icons.people_alt_outlined),iconColor: Colors.blue,
+                  hintText: "Digite seu nome"
+                  
+                  ),
+                  controller: ,
+                  
+
+
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.name, // define o tipo de entrada do teclado
+                  decoration: InputDecoration(border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  icon: Icon(Icons.key),iconColor: Colors.blue,
+                  hintText: "Digite sua senha"
+                  
+                  ),
+                  
+
+
+                ),
+
+              ],
+            ),
+
+          ),
+
+        ],
+
+      ),
+    );
   }
 }
