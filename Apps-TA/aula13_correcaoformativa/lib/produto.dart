ef8dcaeb2344d129172ca7bc3fb3acd2415e8b41
danielvieira95 <engdanielvieira95@gmail.com>
2024-04-25 21:47:02 -0300

    classe produto item

--- Apps-TA/aula13_correcaoformativa/lib/produto.dart
+++ Apps-TA/aula13_correcaoformativa/lib/produto.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 class Produtos extends StatefulWidget {
   const Produtos({super.key});
 
@@ -8,6 +9,12 @@ class Produtos extends StatefulWidget {
 }
 
 class _ProdutosState extends State<Produtos> {
+  _leituradados() async{
+
+    String url = "http://10.109.83.10:3000/produtos";
+    http.Response resposta = await http.get(Uri.parse(url));
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,5 +27,16 @@ class _ProdutosState extends State<Produtos> {
         ],
       ),
     );
+  }
+}
+
+class Produto_item{
+  String id;
+  String nome;
+  String valor;
+  String qtde;
+  Produto_item(this.id, this.nome, this.valor, this.qtde);
+  factory Produto_item.fromJson(Map<String, dynamic> json){
+    return Produto_item(json["id"],json["nome"],json["valor"],json["qtde"]);
   }
 }
