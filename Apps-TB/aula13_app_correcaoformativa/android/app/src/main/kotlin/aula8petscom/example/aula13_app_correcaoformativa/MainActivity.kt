630ac0350a2fb01b988afa60dd202f0fe5e80c2c
danielvieira95 <engdanielvieira95@gmail.com>
2024-04-30 19:05:51 -0300

    app formativa correção

---
+++ Apps-TB/aula13_app_correcaoformativa/android/app/src/main/kotlin/aula8petscom/example/aula13_app_correcaoformativa/MainActivity.kt
@@ -0,0 +1,6 @@
+package aula8petscom.example.aula13_app_correcaoformativa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Apps-TB/aula13_app_correcaoformativa/lib/login.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart'; // pacotes do widget
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold faz parte do layout do app
+    return Scaffold(
+      // body corpo do scaffold
+      body: Column(
+        children: [
+          // elemento decorativo
+          SizedBox(
+            height:300 ,
+            width: 300,
+            child: Column(
+              children: [
+                // textformfield para permitir que o usuário digite seu nome
+                TextFormField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                  icon: Icon(Icons.people_alt_outlined)),// icone do textformfield
+                ),
+              ],
+            ),
+
+          )
+        ],
+      ),
+    );
+  }
+}
