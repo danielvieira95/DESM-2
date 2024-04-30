c258c79abc5b7e0edca57c0515c4533ac87ac5a3
danielvieira95 <engdanielvieira95@gmail.com>
2024-04-30 19:29:27 -0300

    app formativa
    icone button

--- Apps-TB/aula13_app_correcaoformativa/lib/login.dart
+++ Apps-TB/aula13_app_correcaoformativa/lib/login.dart
@@ -10,43 +10,65 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   TextEditingController user = TextEditingController();
   TextEditingController senha = TextEditingController();
+  bool exibir = false; // variavel para exibir ou nao a senha
   @override
   Widget build(BuildContext context) {
     // Scaffold faz parte do layout do app
     return Scaffold(
       // body corpo do scaffold
-      body: Column(
-        children: [
-          // elemento decorativo
-          SizedBox(
-            height:300 ,
-            width: 300,
-            child: Column(
-              children: [
-                // textformfield para permitir que o usuário digite seu nome
-                TextFormField(
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
-                  icon: Icon(Icons.people_alt_outlined,color: Colors.blue,),
-                  hintText: "Digite seu nome"),// Mensagem no text form field
-                  // icone do textformfield
-                  controller: user,
-                 
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
-                  icon: Icon(Icons.key_outlined,color: Colors.blue,),
-                  hintText: "Digite sua senha"),// Mensagem no text form field
-                  // icone do textformfield
-                  controller: senha,
-                 
-                ),
-              ],
-            ),
-
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // elemento decorativo
+            SizedBox(
+              height:300 ,
+              width: 300,
+              child: Column(
+                children: [
+                  // textformfield para permitir que o usuário digite seu nome
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                      icon: Icon(Icons.people_alt_outlined,color: Colors.blue,),
+                      hintText: "Digite seu nome"),// Mensagem no text form field
+                      // icone do textformfield
+                      controller: user,
+                     
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                      icon: Icon(Icons.key_outlined,),iconColor: Colors.blue,
+                      // suffix icon responsável por criar um icone no textformfield
+                      // Icon button icone para o botão 
+                      // exibir ? operador ternário
+                      // if( exibir == true icons.visibility  else visibility off)
+                      suffixIcon: IconButton(icon:  Icon(exibir ? Icons.visibility :Icons.visibility_off),onPressed: (){
+                        setState(() {
+                          exibir = !exibir; // inverte o estado da variavel exibir
+                        });
+                      }),
+                      hintText: "Digite sua senha"),
+                      obscureText: exibir,
+                      obscuringCharacter: '*',// Mensagem no text form field
+                      // icone do textformfield
+                      
+                      controller: senha,
+                     
+                    ),
+                  ),
+                ],
+              ),
+      
+            )
+          ],
+        ),
       ),
     );
   }
