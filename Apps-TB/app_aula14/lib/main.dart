3c1243abf6941faf845a5ea1493fec7ccd44965a
danielvieira95 <engdanielvieira95@gmail.com>
2024-05-07 19:03:48 -0300

    app aula 14 turma b

---
+++ Apps-TB/app_aula14/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("APP BD"),
+      ),
+      body: Column(
+        children: [
+          Text("App Banco de dados"),
+        ],
+      ),
+    );
+  }
+}
