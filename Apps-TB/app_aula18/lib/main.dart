28a2dbbab4f9de775064b59eb7dc0ea66be95e14
danielvieira95 <engdanielvieira95@gmail.com>
2024-06-04 20:38:38 -0300

    app aula 18

--- Apps-TB/app_aula18/lib/main.dart
+++ Apps-TB/app_aula18/lib/main.dart
@@ -3,31 +3,39 @@ import 'package:animated_splash_screen/animated_splash_screen.dart'; // pacote q
 import 'package:page_transition/page_transition.dart'; // pacote para transição de pagina 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: AnimatedSplashScreen(
-      duration: 10000, // tempo de duração da animação
+      duration: 1000, // tempo de duração da animação
       //splash: Icons.home, // icone que será animado
       splash: Image.asset('imagens/senai.png'),
+      splashIconSize: 400,
       nextScreen: Telaprincipal(), // chama a próxima tela
       //splashTransition: SplashTransition.fadeTransition, // transição splash
       //splashTransition: SplashTransition.scaleTransition, // transição splash
       splashTransition: SplashTransition.rotationTransition, // transição splash
       pageTransitionType: PageTransitionType.bottomToTop, // transição para a proxima tela
-      backgroundColor: Colors.purple, // cor do fundo da tela
+      backgroundColor: Colors.red, // cor do fundo da tela
     ),
   ));
 }
 
-class Telaprincipal extends StatelessWidget {
-  const Telaprincipal({super.key});
+class Telaprincipal extends StatefulWidget {
+  @override
+  State<Telaprincipal> createState() => _TelaprincipalState();
+}
+
+class _TelaprincipalState extends State<Telaprincipal> {
+  TextEditingController numero = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false, // elimina erro de overflow
       appBar: AppBar(
         title: Text("App aula 18"
         
         ),
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.redAccent,
       ),
       body: Center(
         child: Column(
@@ -40,7 +48,14 @@ class Telaprincipal extends StatelessWidget {
             }, child: Text("Mensagem")),
             Container(width: 200,height: 200,child: Image.asset('imagens/senai.png',fit: BoxFit.fill,)),
             // fit escala da imagem
-        
+            TextFormField(
+              keyboardType: TextInputType.datetime,
+              controller: numero,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.datetime,
+              controller: numero,
+            )
           ],
         
         ),
